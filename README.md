05d8eab720df1a9b12443af6c11b5cc96d60c02e
MartinBINARD <martin.binard.dev@gmail.com>
2023-12-08 23:52:15 +0100

    Add mini-css-extract-plugin to fix loading css

--- README.md
+++ README.md
@@ -11,10 +11,6 @@
 - This is a portfolio made with vanilla JS, Babel & webpack that uses the KISS principle :raised_hands:.
 - No subdomain for french content and english content is already set in HTML for SEO. This is why english content is not loaded by default.
 
-## Change done
-
-- Change color constrast of buttons and links + Reduce time transition to 0.2s.
-
 ## License
 
 Source code is licensed under the [MIT license](http://opensource.org/licenses/mit-license.php).
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
@@ -19,11 +20,12 @@ module.exports = {
       },
       {
         test: /\.scss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin({
       patterns: [
