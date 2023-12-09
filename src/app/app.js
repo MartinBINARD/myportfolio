421eee08d7ce434d384b073362390f5482deeb6a
MartinBINARD <martin.binard.dev@gmail.com>
2023-12-09 16:14:07 +0100

    Add lang folder and split french &  english speech

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,6 @@
 import { controlButtons } from "./control-buttons.js";
 import { toggleLightMode } from "./light-mode.js";
-import { toggleLanguage } from "./lang.js";
+import { toggleLanguage } from "./lang";
 import { submitBtnLoader } from "./contact-form";
 
 controlButtons();
---
+++ src/app/lang/frenchSpeech.js
@@ -0,0 +1,134 @@
+export const frenchSpeech = [
+  {
+    id: "#greeting",
+    content: "Bonjour, je suis",
+  },
+  {
+    id: "#my-description-part1",
+    content:
+      "Je suis un développeur Web Frontend principalement compétent dans les frameworks Javascript, ",
+  },
+  {
+    id: "#my-description-part2",
+    content: " et j'ai aussi des compétences en NodeJS et serveur Express.",
+  },
+  {
+    id: "#about-left",
+    content: "A propos de ",
+  },
+  {
+    id: "#about-right",
+    content: "A propos de ",
+  },
+  {
+    id: "#about-subtitle-left",
+    content: "informations",
+  },
+  {
+    id: "#about-subtitle-right",
+    content: " me concernant",
+  },
+  {
+    id: "#about-description",
+    content:
+      "Après avoir contribué au développement du logiciel Archibald sous Quasar, Vuetify, NodeJS, Sequelize et MySQL. Je souhaite apporter mon expertise en VueJs et React avec TypeScript et approfondir mes compétences frontend.",
+  },
+  {
+    id: "#frontend-title",
+    content: "Frontend React VueJS & Quasar frameworks Javascript",
+  },
+  {
+    id: "#frontend-description",
+    content:
+      "J'ai contribué à l'ajout de fonctionnalités d'une application réactive et dynamique avec un framework Javascript tel que Quasar basé sur VueJS. Je maîtrise React en utilisant TypeScript et Redux.",
+  },
+  {
+    id: "#backend-description",
+    content:
+      "J'ai contribué à l'ajout de fonctionnalités d'une application réactive et dynamique avec un framework Javascript tel que Quasar basé sur VueJS. Je maîtrise React en utilisant TypeScript et Redux.",
+  },
+  {
+    id: "#linux-title",
+    content: "Environnements Linux et git en ligne de commande",
+  },
+  {
+    id: "#linux-description",
+    content:
+      "Je connais les commandes UNIX et j'utilise une distribution Linux sur mon ordinateur quotidien. Je peux utiliser Git et Github en utilisant la ligne de commande dans un projet collaboratif.",
+  },
+  {
+    id: "#portfolio-title-left",
+    content: "Mon ",
+  },
+  {
+    id: "#cachetrek-title",
+    content: "Application de géocachettes",
+  },
+  {
+    id: "#cachetrek-description",
+    content: "CacheTrek, une application de jeu de géocachettes ludique...",
+  },
+  {
+    id: "#groupomania-title",
+    content: "Réseau social privé",
+  },
+  {
+    id: "#groupomania-description",
+    content:
+      "Groupomania, un réseau social privé avec privilèges d'administrateur...",
+  },
+  {
+    id: "#nostromo-title",
+    content: "Invite de commande pour modèle React",
+  },
+  {
+    id: "#nostromo-description",
+    content:
+      "Nostromo, un modèle React avec TypeScript, pour déployer facilement un nouveau projet...",
+  },
+  {
+    id: "#contact-title-left",
+    content: "contactez ",
+  },
+  {
+    id: "#contact-title-right",
+    content: "moi ",
+  },
+  {
+    id: "#contact-subtitle-left",
+    content: "Laissez-moi",
+  },
+  {
+    id: "#contact-subtitle-right",
+    content: " un message",
+  },
+  {
+    id: "#contact-description",
+    content:
+      "J'ai hâte d'apporter ma contribution au sein d'une équipe de développement et de mettre mes compétences à votre service.",
+  },
+  {
+    id: "#language-title",
+    content: "Langues :",
+  },
+  {
+    id: "#language-description",
+    content: "Anglais, Français",
+  },
+  {
+    id: "email",
+    content: "Votre email...",
+  },
+  {
+    id: "name",
+    content: "Votre Nom & Prénom...",
+  },
+  {
+    id: "text",
+    content: "Votre message...",
+  },
+  {
+    id: "#send-message-btn",
+    content: "Envoyer le message",
+  },
+];
