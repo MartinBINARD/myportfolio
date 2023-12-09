35ab0894242822d589b12ab6d57d0cb1380641f7
MartinBINARD <martin.binard.dev@gmail.com>
2023-12-09 16:40:59 +0100

    Add english speech

---
+++ src/app/lang/englishSpeech.js
@@ -0,0 +1,133 @@
+export const englishSpeech = [
+  {
+    id: "#greeting",
+    content: "Hi, I'm",
+  },
+  {
+    id: "#my-description-part1",
+    content:
+      "I'm a Frontend Web Developer mainly skilled in Javascript frameworks, ",
+  },
+  {
+    id: "#my-description-part2",
+    content: " and also having skills in NodeJS and Express server.",
+  },
+  {
+    id: "#download",
+    content: "Download CV",
+  },
+  {
+    id: "#about-left",
+    content: "About ",
+  },
+  {
+    id: "#about-right",
+    content: "me",
+  },
+  {
+    id: "#about-subtitle-right",
+    content: " about me",
+  },
+  {
+    id: "#about-description",
+    content:
+      "After contributing to the development of Archibald software under Quasar, Vuetify, NodeJS, Sequelize and MySQL. I want to bring my expertise in VueJs and React with TypeScript and deepen my frontend skills",
+  },
+  {
+    id: "#frontend-title",
+    content: "Frontend React VueJS & Quasar Javascript frameworks",
+  },
+  {
+    id: "#frontend-description",
+    content:
+      "I contributed to adding functionalities of a reactive and dynamic application with a Javascript framework such as Quasar based on VueJS. I'm proficient in React with the use of TypeScript and Redux.",
+  },
+  {
+    id: "#backend-description",
+    content:
+      "if you need to develop a REST API, i master the framework NodeJS, Express server with MongoDB or MySQL database through Sequelize library.",
+  },
+  {
+    id: "#linux-title",
+    content: "Linux environments and git in command line",
+  },
+  {
+    id: "#linux-description",
+    content:
+      "I'm familiar with UNIX commands and use a Linux distribution on my daily computer. I'm able to use Git and Github using the command line in collaborative project.",
+  },
+  {
+    id: "#portfolio-title-left",
+    content: "My ",
+  },
+  {
+    id: "#cachetrek-title",
+    content: "Geocaching app",
+  },
+  {
+    id: "#cachetrek-description",
+    content: "CacheTrek, a fun geocaching game app...",
+  },
+  {
+    id: "#groupomania-title",
+    content: "Private Social Network",
+  },
+  {
+    id: "#groupomania-description",
+    content: "Groupomania, a private social network with admin privileges...",
+  },
+  {
+    id: "#nostromo-title",
+    content: "React modele prompt",
+  },
+  {
+    id: "#nostromo-description",
+    content:
+      "Nostromo, a React modele prompt with TypeScript, to easily deploy a new project...",
+  },
+  {
+    id: "#contact-title-left",
+    content: "contact ",
+  },
+  {
+    id: "#contact-title-right",
+    content: "me ",
+  },
+  {
+    id: "#contact-subtitle-left",
+    content: "Drop me",
+  },
+  {
+    id: "#contact-subtitle-right",
+    content: " a message",
+  },
+  {
+    id: "#contact-description",
+    content:
+      "I look forward to make my contribution within a development team and put my skills at your service.",
+  },
+  {
+    id: "#language-title",
+    content: "Languages :",
+  },
+  {
+    id: "#language-description",
+    content: "English, French",
+  },
+  {
+    id: "#input-email",
+    content: "Your email...",
+  },
+  {
+    id: "#input-name",
+    content: "Your Lastname & Firstname...",
+  },
+  {
+    id: "#input-message",
+    content: "Your message...",
+  },
+  {
+    id: "#send-message-btn",
+    content: "Send message",
+  },
+];
