202b37af63253eba73863940faea877f95b96f91
MartinBINARD <martin.binard.dev@gmail.com>
2024-09-11 19:14:48 -1000

    Remove onClicjControlButtons function and save-img/ folder

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,7 @@
-import { onClickControlButtons } from "./onClickControlButtons.js";
 import { handleLightMode } from "./handleLightMode.js";
 import { handleLanguageMenu } from "./lang";
 import { onClickSubmitLoader } from "./onClickSubmitLoader.js";
 
-onClickControlButtons();
 handleLightMode();
 handleLanguageMenu();
 onClickSubmitLoader();
