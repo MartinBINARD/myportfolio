639224f2007635ed254089719c2f83b7e78129e8
MartinBINARD <martin.binard.dev@gmail.com>
2024-09-12 18:32:51 -1000

    Add necessary lucid icons in app.js

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,5 @@
+import { createIcons, Github, Linkedin, Menu, Twitter, X } from 'lucide';
 import { handleHeaderNavMenu } from "./header";
 
+createIcons({ icons: { Menu, X, Github, Linkedin, Twitter }});
 handleHeaderNavMenu()
