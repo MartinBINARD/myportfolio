650db9231b9636b9db89825ffcb0cc2816fe119f
MartinBINARD <martin.binard.dev@gmail.com>
2024-09-13 16:58:39 -1000

    Add GeoCacheTrek project

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,6 @@
-import { createIcons, Github, Linkedin, Menu, Twitter, X } from 'lucide';
+import { Chrome, createIcons, Github, Linkedin, Menu, Twitter, X, Youtube } from 'lucide';
 import { handleHeaderNavMenu } from "./header";
 
-createIcons({ icons: { Menu, X, Github, Linkedin, Twitter }});
+createIcons({ icons: { Menu, X, Github, Linkedin, Twitter, Chrome, Youtube }});
 
 handleHeaderNavMenu()
