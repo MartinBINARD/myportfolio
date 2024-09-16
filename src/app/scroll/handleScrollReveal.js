708727294602a27545b16a8814df0e3ac4c03b90
MartinBINARD <martin.binard.dev@gmail.com>
2024-09-15 23:01:15 -1000

    Add handleScrollReveal function

---
+++ src/app/scroll/handleScrollReveal.js
@@ -0,0 +1,16 @@
+import ScrollReveal from 'scrollreveal';
+
+export const handleScrollReveal = () => {
+  const scroll = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 300,
+    delay: 300,
+  });
+
+  scroll.reveal(`.home__data, .home__social, .contact__container, .footer__container`);
+  scroll.reveal(`.home__image`, {origin: 'bottom'});
+  scroll.reveal(`.about__data, .skills__data`, {origin: 'left'});
+  scroll.reveal(`.about__image, .skills__content`, {origin: 'right'});
+  scroll.reveal(`.projects__card`, {interval: 100});
+};
--- src/app/scroll/index.js
+++ src/app/scroll/index.js
@@ -1,7 +1,9 @@
+import { handleScrollReveal } from "./handleScrollReveal";
 import { setActiveLinkSectionOnScroll } from "./setActiveLinkSectionOnScroll";
 import { showScrollUpOnScroll } from "./showScrollUpOnScroll";
 
 export const handleScroll = () => {
   showScrollUpOnScroll();
   setActiveLinkSectionOnScroll();
+  handleScrollReveal();
 };
