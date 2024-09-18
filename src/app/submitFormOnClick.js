b38538e90ca32241c2eef9a0af948399fe799a44
MartinBINARD <martin.binard.dev@gmail.com>
2024-09-17 22:18:30 -1000

    Add loader on submit on form button + remove onClickSubmitLoader function

--- src/app/submitFormOnClick.js
+++ src/app/submitFormOnClick.js
@@ -3,22 +3,31 @@ import emailjs from '@emailjs/browser';
 export const submitFormOnClick = () => {
   const contactForm = document.getElementById('contact-form');
   const contactMessage = document.getElementById('contact-message');
+  const contactSubmitButton = document.getElementById('contact-submit');
+  const contactSubmitButtonText = document.getElementById('contact-submit__text');
 
   const sendEmail = (e) => {
     e.preventDefault();
-
+    contactSubmitButton.classList.add('loading');
+    contactSubmitButtonText.classList.add('contact__button__text--loading');
+    
     emailjs.sendForm(process.env.serviceID, process.env.templateID, '#contact-form', process.env.publicKey)
-      .then(() => {
-        contactMessage.textContent = 'Message envoyé avec succès ✅';
+    .then(() => {
+      contactMessage.textContent = 'Message envoyé avec succès ✅';
 
         setTimeout(() => {
           contactMessage.textContent = '';
         }, 5000)
-
+        
         contactForm.reset();
       },
       () => {
         contactMessage.textContent = 'Message non envoyé ❌';
+      })
+      .finally(() => {
+        contactSubmitButton.classList.remove('loading');
+        contactSubmitButtonText.classList.remove('contact__button__text--loading');
+        ('loading');
       })
   };
 
