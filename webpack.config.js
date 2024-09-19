7edffa785a274ea8c2a115bb63c6defadd4ecf3f
MartinBINARD <martin.binard.dev@gmail.com>
2024-09-18 21:46:08 -1000

    Add contenthash to filename bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   entry: path.resolve(__dirname, "src/index.js"),
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
+    filename: "[name].[contenthash].bundle.js",
   },
   module: {
     rules: [
